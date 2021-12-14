a813d3055fd5ae09ba81f4f96608518adb8800e0
Manas Vijaywargiya <manasdevslopes@gmail.com>
2021-12-14 17:29:48 +0530

    first commit

---
+++ SwiftUIAccessory/SwiftUIAccessoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAccessoryApp.swift
+//  SwiftUIAccessory
+//
+//  Created by MANAS VIJAYWARGIYA on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAccessoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

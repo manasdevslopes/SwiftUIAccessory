da565fa540667bb6ac965ba0c95a2defa4dc73e4
Manas Vijaywargiya <manasdevslopes@gmail.com>
2021-12-15 16:25:21 +0530

    types of Date Pickers

---
+++ SwiftUIAccessory/DatePickers/DatePickers.swift
@@ -0,0 +1,20 @@
+//
+//  DatePicker.swift
+//  SwiftUIAccessory
+//
+//  Created by MANAS VIJAYWARGIYA on 15/12/21.
+//
+
+import SwiftUI
+
+struct DatePickers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DatePickers_Previews: PreviewProvider {
+    static var previews: some View {
+        DatePickers()
+    }
+}

7ea6ca6ee7d1ec11d3347d350333844f378e39df
Manas Vijaywargiya <manasdevslopes@gmail.com>
2021-12-15 16:25:49 +0530

    types of Date Pickers

--- SwiftUIAccessory/DatePickers/DatePickers.swift
+++ SwiftUIAccessory/DatePickers/DatePickers.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct DatePickers: View {
+    @State private var birthdatepre = Date()
+    @State private var birthdatefu = Date()
+    @State private var birthtimepre = Date()
+    @State private var birthtimefu = Date()
+    @State private var birthdatetimepre = Date()
+    @State private var birthdatetimefu = Date()
+    @State private var birth = Date()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Form {
+                DatePicker("Birthdatetime future", selection: $birth)
+                    .datePickerStyle(.wheel)
+                // Or,
+                DatePicker("Select event date and time", selection: $birth, displayedComponents: [.date, .hourAndMinute])
+                    .datePickerStyle(.compact)
+            }
+            ScrollView {
+                VStack {
+                    DatePicker("Birthdate previous dates only", selection: $birthdatepre, in: ...Date(), displayedComponents: .date)
+                    
+                    DatePicker("Birthdate future dates only", selection: $birthdatefu, in: Date()..., displayedComponents: .date)
+                    
+                    DatePicker("Birthtime previous time only", selection: $birthtimepre, in: ...Date(), displayedComponents: .hourAndMinute)
+                    
+                    DatePicker("Birthtime future time only", selection: $birthtimefu, in: Date()..., displayedComponents: .hourAndMinute)
+                    
+                    DatePicker("Birthdatetime previous", selection: $birthdatetimepre, in: ...Date())
+                        .datePickerStyle(.automatic)
+                    
+                    DatePicker("Birthdatetime future", selection: $birthdatetimefu, in: ...Date())
+                        .datePickerStyle(.graphical)
+                }
+                
+                
+            }
+        }
     }
 }
 

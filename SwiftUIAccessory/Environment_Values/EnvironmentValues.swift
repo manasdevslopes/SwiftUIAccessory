22813121e172e0727dbab2d3e41e955ad2995ef5
Manas Vijaywargiya <manasdevslopes@gmail.com>
2021-12-16 17:47:58 +0530

    environment values

---
+++ SwiftUIAccessory/Environment_Values/EnvironmentValues.swift
@@ -0,0 +1,50 @@
+//
+//  EnvironmentValues.swift
+//  SwiftUIAccessory
+//
+//  Created by MANAS VIJAYWARGIYA on 16/12/21.
+//
+
+import SwiftUI
+
+struct EnvironmentValues: View {
+    @Environment(\.dynamicTypeSize) var dynamicTypeSize
+    @Environment(\.colorScheme) var colorScheme
+    
+    var body: some View {
+        if dynamicTypeSize >= .accessibility2 {
+            VStack {
+                Image("Manas")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                
+                Text("Manas Vijaywargiya")
+                    .bold()
+                    .font(.title)
+                    .foregroundColor(colorScheme == .dark ? .red : .green)
+            }
+        } else {
+            HStack {
+                Image("Manas")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                
+                Text("Manas Vijaywargiya")
+                    .bold()
+                    .font(.title)
+                    .foregroundColor(colorScheme == .dark ? .red : .green)
+            }
+        }
+        
+    }
+}
+
+struct EnvironmentValues_Previews: PreviewProvider {
+    static var previews: some View {
+        EnvironmentValues()
+            .preferredColorScheme(.light)
+            .environment(\.dynamicTypeSize, .accessibility2)
+    }
+}

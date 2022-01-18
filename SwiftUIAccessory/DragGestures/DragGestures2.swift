d5022f068cfe9f30188a93440860bf5b904bcfef
Manas Vijaywargiya <manasdevslopes@gmail.com>
2022-01-18 17:52:33 +0530

    drag gestures

---
+++ SwiftUIAccessory/DragGestures/DragGestures2.swift
@@ -0,0 +1,85 @@
+//
+//  DragGestures2.swift
+//  SwiftUIAccessory
+//
+//  Created by MANAS VIJAYWARGIYA on 18/01/22.
+//
+
+import SwiftUI
+
+struct DragGestures2: View {
+    @State var startingOffsetY: CGFloat = UIScreen.main.bounds.height * 0.85
+    @State var currentDragOffsetY: CGFloat = 0
+    @State var endingOffsetY: CGFloat = 0
+    
+    var body: some View {
+        ZStack {
+            Color.green.ignoresSafeArea()
+            
+            MySignUpView()
+                .offset(y: startingOffsetY)
+                .offset(y: currentDragOffsetY)
+                .offset(y: endingOffsetY)
+                .gesture(
+                    DragGesture()
+                        .onChanged {value in
+                            withAnimation(.spring()) {
+                                currentDragOffsetY = value.translation.height
+                            }
+                        }
+                        .onEnded {value in
+                            withAnimation(.spring()) {
+                                if currentDragOffsetY < -150 {
+                                    endingOffsetY = -startingOffsetY
+                                } else if endingOffsetY != 0 && currentDragOffsetY > 150 {
+                                    endingOffsetY = 0
+                                }
+                                currentDragOffsetY = 0
+
+                            }
+                        }
+                )
+            
+            Text("\(currentDragOffsetY)")
+        }
+        .ignoresSafeArea(edges: .bottom)
+    }
+}
+
+struct DragGestures2_Previews: PreviewProvider {
+    static var previews: some View {
+        DragGestures2()
+            .preferredColorScheme(.light)
+    }
+}
+
+struct MySignUpView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Image(systemName: "chevron.up")
+                .padding(.top)
+            Text("Sign up")
+                .font(.headline)
+                .fontWeight(.semibold)
+            
+            Image(systemName: "flame.fill")
+                .resizable().scaledToFit()
+                .frame(width: 100, height: 100)
+            
+            Text("This is the description of our app. This is my favourite SwiftUI course and i recommend to all of my friends to subscribe to Swiftful Thinking!")
+                .multilineTextAlignment(.center)
+            
+            Text("CREATE AN ACCOUNT")
+                .foregroundColor(.white)
+                .font(.headline)
+                .padding()
+                .padding(.horizontal)
+                .background(Color.black.cornerRadius(10))
+            
+            Spacer()
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color.yellow)
+        .cornerRadius(30)
+    }
+}

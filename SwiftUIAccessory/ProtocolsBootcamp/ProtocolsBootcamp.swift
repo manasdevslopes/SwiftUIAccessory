11f89c3497acb906440b1788171094441611d53b
Manas Vijaywargiya <manasdevslopes@gmail.com>
2022-03-24 18:45:52 +0530

    How to use Protocols

---
+++ SwiftUIAccessory/ProtocolsBootcamp/ProtocolsBootcamp.swift
@@ -0,0 +1,90 @@
+//
+//  ProtocolsBootcamp.swift
+//  SwiftUIAccessory
+//
+//  Created by MANAS VIJAYWARGIYA on 24/03/22.
+//
+
+import SwiftUI
+
+struct DefaultColorTheme: ColorThemeProtocol {
+    let primary: Color = .blue
+    let secondary: Color = .white
+    let tertiary: Color = .gray
+}
+
+struct AlternativeColorTheme: ColorThemeProtocol {
+    let primary: Color = .red
+    let secondary: Color = .white
+    let tertiary: Color = .green
+}
+
+struct AnotherColorTheme: ColorThemeProtocol {
+    let primary: Color = .blue
+    let secondary: Color = .red
+    let tertiary: Color = .purple
+}
+
+protocol ColorThemeProtocol {
+    var primary: Color { get }
+    var secondary: Color { get }
+    var tertiary: Color { get }
+}
+
+protocol ButtonTextProtocol {
+    var buttonText: String { get }
+}
+
+protocol ButtonPressedProtocol {
+    func buttonPressed()
+}
+
+protocol ButtonDataSourceProtocol: ButtonTextProtocol, ButtonPressedProtocol {
+    // protocols inherit from other protocols too
+}
+class DefaultDataSource: ButtonDataSourceProtocol {
+    var buttonText: String = "Protocols are awesome!"
+    
+    func buttonPressed() {
+        print("Button was pressed!")
+    }
+}
+
+class AlternativeDataSource: ButtonTextProtocol {
+    var buttonText: String = "Protocols are lame!"
+}
+
+struct ProtocolsBootcamp: View {
+    // let colorTheme: DefaultColorTheme = DefaultColorTheme() // instance
+    // let colorTheme: AlternativeColorTheme = AlternativeColorTheme() // instance
+    
+    // let colorTheme: ColorThemeProtocol = DefaultColorTheme()
+    // Or, let colorTheme: ColorThemeProtocol = AlternativeColorTheme()
+    let colorTheme: ColorThemeProtocol
+    
+    let dataSource: ButtonDataSourceProtocol
+//    let dataSource2: ButtonPressedProtocol
+    
+    var body: some View {
+        ZStack {
+            colorTheme.tertiary
+                .ignoresSafeArea()
+            Text(dataSource.buttonText)
+                .font(.headline)
+                .foregroundColor(colorTheme.secondary)
+                .padding()
+                .background(colorTheme.primary)
+                .cornerRadius(10)
+                .onTapGesture {
+                    dataSource.buttonPressed()
+                }
+            
+        }
+    }
+}
+
+struct ProtocolsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ProtocolsBootcamp(colorTheme: DefaultColorTheme(), dataSource: DefaultDataSource())
+    }
+}

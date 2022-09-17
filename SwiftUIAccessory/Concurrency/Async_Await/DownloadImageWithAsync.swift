a756b67629d495eff0a63ff1a169a59ce0130b1e
Manas Vijaywargiya <manasdevslopes@gmail.com>
2022-09-17 17:35:07 +0530

    Concurrent playlist - Part 2 Download Images with Async/Await, @escaping closure, combine framework

--- SwiftUIAccessory/Concurrency/Async_Await/DownloadImageWithAsync.swift
+++ SwiftUIAccessory/Concurrency/Async_Await/DownloadImageWithAsync.swift
@@ -1,46 +1,43 @@
   //
-  //  DownloadImageAsync.swift
+  //  DownloadimageWithAsync.swift
   //  SwiftUIAccessory
   //
-  //  Created by MANAS VIJAYWARGIYA on 11/05/22.
+  //  Created by MANAS VIJAYWARGIYA on 17/09/22.
   //
 
 import SwiftUI
 import Combine
 
-class DownloadImageAsyncImageLoader {
-  static let shared = DownloadImageAsyncImageLoader()
-  private init() {}
+class DownloadImageWithAsyncLoader {
   
-  let url = URL(string: "https://picsum.photos/200")!
+  let url = URL(string: "https://picsum.photos/200/300")!
   
   func handleResponse(data: Data?, response: URLResponse?) -> UIImage? {
     guard
       let data = data,
       let image = UIImage(data: data),
       let response = response as? HTTPURLResponse,
-      response.statusCode >= 200 && response.statusCode < 300  else {
+      response.statusCode >= 200 && response.statusCode < 300 else {
       return nil
     }
     return image
   }
   
-  func downloadWithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> ()) {
+  func downloadImageWithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> ()) {
     URLSession.shared.dataTask(with: url) {[weak self] data, response, error in
       let image = self?.handleResponse(data: data, response: response)
       completionHandler(image, error)
-    }
-    .resume()
+    }.resume()
   }
   
-  func downloadWithCombine() -> AnyPublisher<UIImage?, Error> {
+  func downloadImageWithCombine() -> AnyPublisher<UIImage?, Error> {
     URLSession.shared.dataTaskPublisher(for: url)
       .map(handleResponse)
       .mapError({ $0 })
       .eraseToAnyPublisher()
   }
   
-  func downloadWithAsynAwait() async throws -> UIImage? {
+  func downloadImageWithAsync() async throws -> UIImage? {
     do {
       let (data, response) = try await URLSession.shared.data(from: url, delegate: nil)
       let image = handleResponse(data: data, response: response)
@@ -51,51 +48,53 @@ class DownloadImageAsyncImageLoader {
   }
 }
 
-class DownloadImageAsyncViewModel: ObservableObject {
+class DownloadImageWithAsyncViewModel: ObservableObject {
+  let loader = DownloadImageWithAsyncLoader()
+  
   @Published var image: UIImage? = nil
-  let loader = DownloadImageAsyncImageLoader.shared
   var cancellables =  Set<AnyCancellable>()
   
   func fetchImage() async {
     /* Download With Escaping Function
-     loader.downloadWithEscaping {[weak self] image, error in
+     loader.downloadImageWithEscaping {[weak self] image, error in
+     //    if let image {
      DispatchQueue.main.async {
      self?.image = image
      }
+     //    }
      }
      */
     
     /* Download with Combine
-    loader.downloadWithCombine()
+    loader.downloadImageWithCombine()
       .receive(on: DispatchQueue.main)
       .sink { _ in
-        
-      } receiveValue: {[weak self] image in
+      } receiveValue: { [weak self] image in
         self?.image = image
       }
       .store(in: &cancellables)
-    */
+     */
     
     // Download with Async/Await
     // function needs to be written with async to support concurrency
-    let image = try? await loader.downloadWithAsynAwait()
-    // MARK: - instead of DispatchQueue.main.async {}, use MainActor.run {} in Async/Await example. Actor will be covered in another video of Swiftful thinking
+    let image = try? await loader.downloadImageWithAsync()
     await MainActor.run {
       self.image = image
     }
   }
+  
 }
 
-struct DownloadImageAsync: View {
-  @StateObject private var vm = DownloadImageAsyncViewModel()
+struct DownloadImageWithAsync: View {
+  @StateObject private var viewModel = DownloadImageWithAsyncViewModel()
   
   var body: some View {
     ZStack {
-      if let image = vm.image {
+      if let image = viewModel.image {
         Image(uiImage: image)
           .resizable()
-          .scaledToFit()
-          .frame(width: 250, height: 250)
+          .scaledToFill()
+          .frame(maxWidth: .infinity, maxHeight: .infinity)
           .cornerRadius(25)
       } else {
         VStack {
@@ -107,19 +106,19 @@ struct DownloadImageAsync: View {
         }
       }
     }
+    .ignoresSafeArea()
     .onAppear {
-      // vm.fetchImage()
+      // viewModel.fetchImage()
       
-      // For calling async function
       Task {
-        await vm.fetchImage()
+        await viewModel.fetchImage()
       }
     }
   }
 }
 
-struct DownloadImageAsync_Previews: PreviewProvider {
+struct DownloadImageWithAsync_Previews: PreviewProvider {
   static var previews: some View {
-    DownloadImageAsync()
+    DownloadImageWithAsync()
   }
 }
